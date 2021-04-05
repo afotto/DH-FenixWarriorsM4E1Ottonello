c82d308e9fa260458091385fcd055e336e3e63d9
Andres <andres.ottonello@gmail.com>
2021-04-05 19:20:06 -0300

    heroku

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+// const port = 3000
+const port = process.env.PORT;
 const path = require('path');
 console.log('Entrega obligatoria Andrés Ottonello')
 app.use(express.static('public'));
@@ -22,6 +24,6 @@ app.get('/search', (req, res) => {
     res.sendFile(path.join(__dirname, './views/search.html'));
 });
 
-app.listen(port, () => {
+app.listen(port || 3000, () => {
     console.log('Example app listening on port '+ port);
 });
